38713bec2c7ee95bd42b1473b9226deddf318d7e
frozenbanana <henrybergstrom1@gmail.com>
2017-11-25 16:17:35 +0100

    Added shader files, more models and GLOBAL.h

---
+++ GLOBALS.hpp
@@ -0,0 +1,23 @@
+#ifndef GLOBALS_HPP
+#define GLOBALS_HPP
+#include <GL/glew.h> 
+#include <vector>
+#include <glm/glm.hpp>
+
+const int WINDOW_HEIGHT = 640;
+const int WINDOW_WIDTH = 480;
+const GLfloat PI = 3.1415926535f;
+
+struct ModelData {
+  std::vector<GLuint> s_VAOs;
+  std::vector<std::vector<GLuint> > s_meshIndices;
+  glm::mat4 s_modelMat;
+};
+
+#define COLOR_WHITE glm::vec3(1.0f, 1.0f, 1.0f)
+#define COLOR_RED glm::vec3(1.0f, 0.0f, 0.0f)
+#define COLOR_GREEN glm::vec3(0.0f, 1.0f, 0.0f)
+#define COLOR_CYAN glm::vec3(0.0f, 1.0f, 1.0f)
+#define COLOR_BLUE glm::vec3(0.0f, 0.0f, 1.0f)
+
+#endif
---
+++ res/shaders/base_vs.glsl
@@ -0,0 +1,10 @@
+#version 440
+
+in vec3 vp;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 perspective;
+
+void main() {
+  gl_Position = perspective * view * model * vec4(vp, 1.0);
+}
